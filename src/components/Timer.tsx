208c5b5393e6ada73fd50241945c7bcadd55bbc0
Yuragranovsky <yuragranovsky@gmail.com>
2023-03-08 07:21:00 +0200

    CW #32 parent - child interface (App - Timer)

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,15 +1,27 @@
-import React from "react"
-export const Timer:React.FC = () => {
+import React, {useState, useEffect} from "react";
+import timeZones from "../time-zones";
+type Props = {
+    cityCountry:string;
+}
+export const Timer:React.FC<Props> = ({cityCountry}) => {
 const styles: React.CSSProperties = {backgroundColor:"lightblue",
 fontSize: "2em"};
-setTimeout(tic, 2000);
-const [time, setTime] = React.useState(new Date())
+
+const [time, setTime] = useState<Date>(new Date());
+
 function tic() {
-    setTime(new Date())
+    setTime(new Date());
+    
 }
 
+useEffect(() => {
+    const interval = setInterval(tic, 2000);
+    console.log("useEffect");
+    return () => clearInterval(interval);
+}, [])
+
     return <div>
-        <h2>Current Time</h2>
-        <p style={styles}>{time.toLocaleTimeString()}</p>
+        <h2 >Current Time in {cityCountry}</h2>
+        <p style={styles}>{time.toLocaleTimeString(undefined, {timeZone:timeZones[3].name})}</p>
     </div>
 }
