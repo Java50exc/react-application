bb6760884b6bb61d768194d9e19db4fb7c033f35
Yuragranovsky <yuragranovsky@gmail.com>
2023-03-10 10:29:52 +0200

    HW #32 Solution Four timers

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,20 @@ import './App.css';
 import { Timer } from './components/Timer';
 
 function App() {
-  return <div>
-    <Timer cityCountry="London"/>
-    <Timer cityCountry="Vladivostok"/>
+  const column: React.CSSProperties = {display: "flex", flexDirection: "column",
+  height: "90vh", justifyContent: "center"};
+  const row: React.CSSProperties = {display: "flex", flexDirection: "row",
+   justifyContent: "space-around"};
+  return <div style={column}>
+    <div style={row}>
+       <Timer cityCountry="London"/>
+    <Timer cityCountry="Kishenev"/>
+    </div>
+   <div style={row}>
     <Timer cityCountry="Israel"/>
     <Timer cityCountry="Toronto"/>
+   </div>
+    
     </div>
 }
 
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import timeZones from "../time-zones";
 type Props = {
     cityCountry:string;
@@ -8,20 +8,30 @@ const styles: React.CSSProperties = {backgroundColor:"lightblue",
 fontSize: "2em"};
 
 const [time, setTime] = useState<Date>(new Date());
-
+const timeZone = useRef<string|undefined>();
 function tic() {
     setTime(new Date());
     
 }
+useEffect(
+ () => {
+    timeZone.current = getTimeZone();
+ }, [cityCountry]
+)
 
 useEffect(() => {
     const interval = setInterval(tic, 2000);
     console.log("useEffect");
     return () => clearInterval(interval);
 }, [])
-
+function getTimeZone(): string | undefined{
+    const index = timeZones.findIndex(tz => JSON.stringify(tz).includes(cityCountry));
+    console.log("getTimeZone")
+    return index < 0 ? undefined : timeZones[index].name
+} 
     return <div>
         <h2 >Current Time in {cityCountry}</h2>
-        <p style={styles}>{time.toLocaleTimeString(undefined, {timeZone:timeZones[3].name})}</p>
+        <p style={styles}>{time.toLocaleTimeString(undefined,
+             {timeZone: timeZone.current})}</p>
     </div>
 }
