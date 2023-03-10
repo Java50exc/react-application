92d577f0d86b9da5d1a05f6ce73fc6cb0973dc6e
Yuragranovsky <yuragranovsky@gmail.com>
2023-03-10 13:03:06 +0200

    CW #33 Input & Alert components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
 
 import './App.css';
+import { InputTest } from './components/InputTest';
 import { Timer } from './components/Timer';
 
 function App() {
-  const column: React.CSSProperties = {display: "flex", flexDirection: "column",
-  height: "90vh", justifyContent: "center"};
-  const row: React.CSSProperties = {display: "flex", flexDirection: "row",
-   justifyContent: "space-around"};
-  return <div style={column}>
-    <div style={row}>
-       <Timer cityCountry="London"/>
-    <Timer cityCountry="Kishenev"/>
-    </div>
-   <div style={row}>
-    <Timer cityCountry="Israel"/>
-    <Timer cityCountry="Toronto"/>
-   </div>
-    
-    </div>
+  return <InputTest/>
 }
 
 export default App;
