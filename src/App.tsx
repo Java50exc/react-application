1da46bc1ccb69ce4825f381624d17854635750de
Yuragranovsky <yuragranovsky@gmail.com>
2023-03-28 20:58:35 +0300

    CW #38 React Router functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 
 import React, { ReactNode } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
-import { Life } from './components/Life';
+import { Bread } from './components/pages/Bread';
+import { Customers } from './components/pages/Customers';
+import { Dairy } from './components/pages/Dairy';
+import { Home } from './components/pages/Home';
+import { NotFound } from './components/pages/NotFound';
+import { Orders } from './components/pages/Orders';
+import { Products } from './components/navigators/Products';
+import { ShoppingCart } from './components/pages/ShoppingCart';
+import { Layout } from './components/navigators/Layout';
+
 
 
 function App() {
-    
- return <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-      <Life />
- </div>
+
+     return <BrowserRouter>
+          <Routes>
+               <Route path='/' element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path='customers' element={<Customers />} />
+                    <Route path='orders' element={<Orders />} />
+                    <Route path='shoppingcart' element={<ShoppingCart />} />
+                    <Route path='products' element={<Products />}>
+                         <Route path='dairy' element={<Dairy />} />
+                         <Route path='bread' element={<Bread />} />
+
+                    </Route>
+               </Route>
+
+
+
+
+
+               <Route path='/*' element={<NotFound />} />
+
+          </Routes>
+     </BrowserRouter>
 }
 
 export default App;
