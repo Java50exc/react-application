b0ce65b4de9f2f44684315fa587b4a72c525220c
Yuragranovsky <yuragranovsky@gmail.com>
2023-03-31 11:05:05 +0300

    Fix Navigator

--- src/components/navigators/Navigator.tsx
+++ src/components/navigators/Navigator.tsx
@@ -15,12 +15,17 @@ export const Navigator: React.FC<Props> = ({ subnav, routes }) => {
          }, [])
     function getItems(): ReactNode {
         return routes.map((route, index) =>
-            <li className="navigator-item" key={index}><NavLink to={route.path}>{route.label}</NavLink></li>)
+            <li className="navigator-item" key={index}>
+                <NavLink to={route.path}>{route.label}
+                </NavLink></li>)
     }
     return <div>
-        <ul className={`navigator-list ${subnav ? 'navigator-sublist' : ''}`}>
+        <nav>
+          <ul className={`navigator-list ${subnav ? 'navigator-sublist' : ''}`}>
             {getItems()}
-        </ul>
+        </ul>  
+        </nav>
+        
         <Outlet></Outlet>
     </div>
 }
