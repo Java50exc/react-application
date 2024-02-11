1fb703e31d6cda3f31df1f7e832c19589956e339
allanteone <allanteone@gmail.com>
2024-02-11 23:55:47 +0200

    hw-31 initial commit

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 export const Timer:React.FC = () => {
-const styles: React.CSSProperties = {backgroundColor:"lightblue",
-fontSize: "2em"};
+const styles: React.CSSProperties = {backgroundColor:"lightblue", fontSize: "2em"};
+
 setTimeout(tic, 2000);
+
 const [time, setTime] = React.useState(new Date())
+
 function tic() {
-    setTime(new Date())
+    setTime(new Date());
 }
 
     return <div>
