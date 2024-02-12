41a1e2fd1ead68f2bbfb6462dd53c267646e032e
allanteone <allanteone@gmail.com>
2024-02-12 20:17:48 +0200

    hw-33 fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ import { InputTest } from './components/InputTest';
 import { Timer } from './components/Timer';
 
 function App() {
-
-  const [cityCountry1, setCityCountry1] = useState("Jerusalem");
-  const [cityCountry2, setCityCountry2] = useState("Jerusalem");
-  const [cityCountry3, setCityCountry3] = useState("Jerusalem");
-  const [cityCountry4, setCityCountry4] = useState("Jerusalem");
   return <div>
 
     <Timer cityCountry={"Europe/Vilnius"}/>
