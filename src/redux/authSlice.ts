20890788725a60b857ba0e998175cb014fd10112
allanteone <allanteone@gmail.com>
2024-02-15 10:54:59 +0200

    hw-39 adds authSlice

--- src/redux/authSlice.ts
+++ src/redux/authSlice.ts
@@ -1,5 +1,18 @@
-
-
-const initialValue = {
-    
+import { createSlice } from "@reduxjs/toolkit";
+const initialState: { authUser: string } = {
+    authUser: ""
 }
+const authSlice = createSlice({
+    initialState,
+    name: "auth",
+    reducers: {
+        login: (state, data) => {
+            state.authUser = data.payload;
+        },
+        logout: (state) => {
+            state.authUser = "";
+        }
+    }
+});
+export const authActions = authSlice.actions;
+export const authReducer = authSlice.reducer;
