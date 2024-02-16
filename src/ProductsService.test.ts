76bd8ba8a7d112180848d76e2270444644d34d94
allanteone <allanteone@gmail.com>
2024-02-16 23:11:35 +0200

    hw-42

--- src/ProductsService.test.ts
+++ src/ProductsService.test.ts
@@ -1,15 +1,51 @@
 import { productsService } from "./config/products-service-config"
 import productsConfig from "./config/products-config.json"
+import { getRandomNumber } from "./util/random";
+
+let categories: string[];
+
+beforeAll(() => {
+    categories = productsConfig.map(p => p.name.split("-")[0]);
+});
+
 test("setProducts test", () => {
     productsService.setProducts().then(count => {
-        expect(count).toEqual(productsConfig.length);
-    })
-})
-test ("category bread exists", () => {
+        expect(count).toEqual(1000);
+    });
+});
+
+test("category bread exists", () => {
     productsService.isCategoryExist("bread")
-    .then(res => expect(res).toBeTruthy());
-})
-test ("category kukureku doesn't exist", () => {
+        .then(res => expect(res).toBeTruthy());
+});
+
+test("category kukureku doesn't exist", () => {
     productsService.isCategoryExist("kukureku")
-    .then(res => expect(res).toBeFalsy());
-})
+        .then(res => expect(res).toBeFalsy());
+});
+
+test("random category exists", () => {
+    productsService
+        .isCategoryExist(categories[getRandomNumber(0, categories.length - 1)])
+        .then(res => expect(res).toBeTruthy());
+});
+
+test("all category exists", () => {
+    const promises = categories.map(c => productsService.isCategoryExist(c));
+    Promise.all(promises).then(values => expect(values.every(val => val)).toBeTruthy())
+});
+
+test("remove category", () => {
+    const category = categories[getRandomNumber(0, categories.length - 1)];
+    productsService.removeCategory(category)
+        .then(() => productsService.isCategoryExist(category))
+        .then(res => expect(res).toBeFalsy());
+});
+
+test("add category", () => {
+    const category = categories[getRandomNumber(0, categories.length - 1)] + "123";
+    productsService
+        .addCategory({ name: category })
+        .then(() => productsService.isCategoryExist(category))
+        .then(res => expect(res).toBeFalsy());
+});
