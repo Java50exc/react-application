4004ad4f08cda19b46ebc8e11ec33c1dff452cc1
allanteone <allanteone@gmail.com>
2024-02-18 19:52:25 +0200

    hw-45

--- src/components/pages/ProductsAdmin.tsx
+++ src/components/pages/ProductsAdmin.tsx
@@ -1,22 +1,60 @@
-import { Typography, Box, Avatar } from "@mui/material"
-import {DataGrid, GridColDef} from "@mui/x-data-grid"
-import { ProductType } from "../../model/ProductType"
-import { useSelector } from "react-redux"
+import {Typography, Box, Avatar, Alert, Snackbar} from "@mui/material"
+import {DataGrid, GridActionsCellItem, GridColDef} from "@mui/x-data-grid"
+import {ProductType} from "../../model/ProductType"
+import {useSelector} from "react-redux"
+import {Delete} from "@mui/icons-material";
+import React, {useRef, useState} from "react";
+import {productsService} from "../../config/products-service-config";
+
 export const ProductsAdmin: React.FC = () => {
-    const products: ProductType[] =
-     useSelector<any, ProductType[]>(state => state.productsState.products);
+    const products: ProductType[] = useSelector<any, ProductType[]>(state => state.productsState.products);
+    const [open, setOpen] = useState<boolean>(false);
+    const alertMessage = useRef<string>('');
+
     const columns: GridColDef[] = [
-        {field:"image", headerName: '', flex: 0.3,
-         renderCell: (params) => <Avatar src={`images/${params.value}`} 
-         sx={{width: "90%", height: "80px"}}/>, align: "center", headerAlign: "center"},
+        {
+            field: "image",
+            headerName: '',
+            flex: 0.3,
+            renderCell: (params) => <Avatar src={`images/${params.value}`}
+                                            sx={{width: "90%", height: "80px"}}/>,
+            align: "center",
+            headerAlign: "center"
+        },
         {field: "title", headerName: 'Title', flex: 0.8, align: "center", headerAlign: "center"},
         {field: "category", headerName: "Category", flex: 0.5},
         {field: "unit", headerName: "Unit", flex: 0.4},
-        {field: "cost", headerName: "Cost (ILS)", flex: 0.3}
-    ]
-    return <Box sx={{width: "100vw",display: "flex", justifyContent:"center"}}>
-        <Box sx={{width: "80vw",height: "80vh"}}>
-        <DataGrid columns={columns} rows={products} getRowHeight={() => 'auto'}/>
-    </Box>
+        {field: "cost", headerName: "Cost (ILS)", flex: 0.3, editable: true},
+        {
+            field: 'actions', type: 'actions', flex: 0.1, getActions: (params) => [
+                <GridActionsCellItem label="remove" icon={<Delete></Delete>}
+                                     onClick={async () =>
+                                         await productsService.removeProduct(params.id as string)}/>
+            ]
+        }
+    ];
+
+    async function updateCost(newRow: ProductType, oldRow: ProductType): Promise<any> {
+        if (newRow.cost > (oldRow.cost + oldRow.cost / 2) || newRow.cost < (oldRow.cost - oldRow.cost / 2)) {
+            throw 'Update cannot be greater than on 50% from the existing cost';
+        }
+        await productsService.removeProduct(oldRow.id!);
+        await productsService.addProduct(newRow);
+        return newRow;
+    }
+
+    return <Box sx={{width: "100vw", display: "flex", justifyContent: "center"}}>
+        <Box sx={{width: "80vw", height: "80vh"}}>
+            <DataGrid columns={columns} rows={products} getRowHeight={() => 'auto'}
+                      processRowUpdate={updateCost} onProcessRowUpdateError={(error) => {
+                alertMessage.current = error;
+                setOpen(true)
+            }}/>
+            <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
+                <Alert severity="error" sx={{width: '30vw', fontSize: '1.5em'}}>
+                    {alertMessage.current}
+                </Alert>
+            </Snackbar>
+        </Box>
     </Box>
 }
--- src/components/pages/ShoppingCart.tsx
+++ src/components/pages/ShoppingCart.tsx
@@ -1,11 +1,11 @@
-import { useSelector } from "react-redux"
-import { ProductType } from "../../model/ProductType";
-import { ShoppingProductType } from "../../model/ShoppingProductType";
-import { useMemo, useRef, useState } from "react";
-import { ordersService } from "../../config/orders-service-config";
-import { GridColDef, DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
-import { Avatar, Box, Typography, Snackbar, Alert } from "@mui/material";
-import { Delete } from "@mui/icons-material";
+import {useSelector} from "react-redux"
+import {ProductType} from "../../model/ProductType";
+import {ShoppingProductType} from "../../model/ShoppingProductType";
+import {useMemo, useRef, useState} from "react";
+import {ordersService} from "../../config/orders-service-config";
+import {GridColDef, DataGrid, GridActionsCellItem} from "@mui/x-data-grid";
+import {Avatar, Box, Typography, Snackbar, Alert} from "@mui/material";
+import {Delete} from "@mui/icons-material";
 
 type ShoppingDataType = ProductType & { count: number, price: number }
 export const ShoppingCart: React.FC = () => {
@@ -13,50 +13,52 @@ export const ShoppingCart: React.FC = () => {
     const alertMessage = useRef<string>('');
     const products = useSelector<any, ProductType[]>(state => state.productsState.products);
     const shopping = useSelector<any, ShoppingProductType[]>
-        (state => state.shoppingState.shopping);
+    (state => state.shoppingState.shopping);
     const authUser = useSelector<any, string>(state => state.auth.authUser);
     const columns: GridColDef[] = [
         {
             field: 'image', headerName: '', flex: 0.5, align: 'center', sortable: false,
-             headerAlign: 'center',
-            renderCell: (params) => <Avatar src={`images/${params.value}`} sx={{ width: "50%", height: "12vh" }} />
+            headerAlign: 'center',
+            renderCell: (params) => <Avatar src={`images/${params.value}`} sx={{width: "50%", height: "12vh"}}/>
         },
+        {field: 'title', headerName: 'Title', flex: 1, align: 'center', headerAlign: 'center'},
+        {field: 'unit', headerName: 'Unit', flex: 0.3},
+        {field: 'cost', headerName: 'Cost(ILS)', flex: 0.3, type: 'number'},
+        {field: 'count', headerName: 'Count', flex: 0.2, editable: true, type: 'number'},
+        {field: 'price', headerName: 'Price', flex: 0.3, type: 'number'},
         {
-            field: 'title', headerName: 'Title', flex: 1,
-            align: 'center', headerAlign: 'center'
-        },
-        { field: 'unit', headerName: 'Unit', flex: 0.3 },
-        { field: 'cost', headerName: 'Cost(ILS)', flex: 0.3, type: 'number' },
-        { field: 'count', headerName: 'Count', flex: 0.2, editable: true, type: 'number'  },
-        { field: 'price', headerName: 'Price', flex: 0.3, type: 'number'  },
-        {field: 'actions', type: 'actions', flex: 0.1, getActions: (params) => [
-            <GridActionsCellItem label="remove" icon={<Delete></Delete>}
-             onClick={async () => await 
-                ordersService.removeShoppingProduct(authUser, params.id as string)}/>
-        ]}
-    
+            field: 'actions', type: 'actions', flex: 0.1, getActions: (params) => [
+                <GridActionsCellItem label="remove" icon={<Delete></Delete>}
+                                     onClick={async () => await
+                                         ordersService.removeShoppingProduct(authUser, params.id as string)}/>
+            ]
+        }
+
     ]
     const tableData = useMemo(() => getTableData(), [products, shopping]);
     const total = useMemo(() => getTotalCost(), [tableData]);
+
     async function updateCount(newRow: any): Promise<any> {
         const rowData: ShoppingDataType = newRow;
         if (rowData.count < 1) {
             throw 'count must be greater than 0'
         }
-        await ordersService.addShoppingProduct(authUser,
-            rowData.id!, { id: rowData.id!, count: rowData.count })
+        await ordersService.addShoppingProduct(authUser, rowData.id!, {id: rowData.id!, count: rowData.count})
         return newRow;
     }
+
     function getTotalCost(): number {
         return tableData.reduce((res, cur) => res + cur.price, 0);
     }
+
     function getTableData(): ShoppingDataType[] {
         const shoppingData: ShoppingDataType[] = shopping.map(getShoppingProduct)
         return shoppingData.filter(sd => !!sd.id)
     }
+
     function getShoppingProduct(shoppingProduct: ShoppingProductType): ShoppingDataType {
         const product: ProductType | undefined = products.find
-            (p => shoppingProduct.id == p.id);
+        (p => shoppingProduct.id == p.id);
         let res: ShoppingDataType = {
             id: "", category: '', cost: 0, count: 0,
             title: '', image: '', price: 0, unit: ''
@@ -64,25 +66,27 @@ export const ShoppingCart: React.FC = () => {
         if (!product) {
             ordersService.removeShoppingProduct(authUser, shoppingProduct.id);
         } else {
-            res = { ...product, count: shoppingProduct.count, price: +(product.cost * shoppingProduct.count).toFixed(2) };
+            res = {...product, count: shoppingProduct.count, price: +(product.cost * shoppingProduct.count).toFixed(2)};
         }
         return res;
 
     }
-    return <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '80vh', alignItems: 'center' }}>
-        <Box sx={{ height: '60vh', width: '70vw' }}>
+
+    return <Box
+        sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '80vh', alignItems: 'center'}}>
+        <Box sx={{height: '60vh', width: '70vw'}}>
             <DataGrid columns={columns} rows={tableData} getRowHeight={() => 'auto'}
-                processRowUpdate={updateCount}
-                 onProcessRowUpdateError={(error) => {
-                    alertMessage.current = error;
-                    setOpen(true)
-                 }}/>
+                      processRowUpdate={updateCount}
+                      onProcessRowUpdateError={(error) => {
+                          alertMessage.current = error;
+                          setOpen(true)
+                      }}/>
 
         </Box>
         <Typography variant="h6">Total cost: {total.toFixed(2)}{' '}
-            <img src="images/israeli-shekel-icon.svg" width="3%" /></Typography>
+            <img src="images/israeli-shekel-icon.svg" width="3%"/></Typography>
         <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
-            <Alert severity="error" sx={{ width: '30vw', fontSize: '1.5em' }}>
+            <Alert severity="error" sx={{width: '30vw', fontSize: '1.5em'}}>
                 {alertMessage.current}
             </Alert>
         </Snackbar>
